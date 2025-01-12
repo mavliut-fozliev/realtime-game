df6d12d05b28252d975333d0cb9077da2f768cd3
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-01-12 18:48:10 +0300

    fix: 2 pressed keys will work together

--- src/canvas-entities/player.ts
+++ src/canvas-entities/player.ts
@@ -2,39 +2,53 @@ import { DataSnapshot } from "firebase/database";
 import Entity from "./Entity";
 
 class Player extends Entity {
-  private currentX: number;
-  private currentY: number;
+  private keyState: { [key: string]: boolean } = {};
+  private movement: { x: number; y: number } = { x: 0, y: 0 };
 
   constructor(private ctx: CanvasRenderingContext2D, private x = 0, private y = 0) {
-    super(
-      "player",
-      (snapshot: DataSnapshot) => {
-        const playerData = snapshot.val();
+    const documentKey = "player";
 
-        if (playerData.position.x !== undefined) {
-          this.x = playerData.position.x;
-        }
-        if (playerData.position.y !== undefined) {
-          this.y = playerData.position.y;
-        }
-      },
-      {
-        "ArrowRight": () => this.move("x", "+"),
-        "ArrowDown": () => this.move("y", "+"),
-        "ArrowLeft": () => this.move("x", "-"),
-        "ArrowUp": () => this.move("y", "-"),
+    const onSnapshot = (snapshot: DataSnapshot) => {
+      const playerData = snapshot.val();
+      if (playerData && playerData.position) {
+        this.x = playerData.position.x || this.x;
+        this.y = playerData.position.y || this.y;
       }
-    );
+    };
 
-    this.currentX = this.x;
-    this.currentY = this.y;
+    const keyHandlers = {
+      "ArrowRight": () => this.move("x", "+"),
+      "ArrowLeft": () => this.move("x", "-"),
+      "ArrowUp": () => this.move("y", "-"),
+      "ArrowDown": () => this.move("y", "+"),
+    };
+
+    super(documentKey, onSnapshot, keyHandlers);
+
+    this.movement = { x: this.x, y: this.y };
+
+    window.addEventListener("keydown", (event: KeyboardEvent) => {
+      this.keyState[event.key] = true;
+      this.updateMovement();
+    });
+
+    window.addEventListener("keyup", (event: KeyboardEvent) => {
+      this.keyState[event.key] = false;
+      this.updateMovement();
+    });
   }
 
   private move(axis: "x" | "y", direction: "+" | "-") {
     const delta = direction === "+" ? 1 : -1;
-    this[axis] += delta;
+    this.movement[axis] += delta;
+    this.updateValue("player/position", axis, this.movement[axis]);
+  }
 
-    this.updateValue("player/position", axis, this[axis]);
+  private updateMovement() {
+    if (this.keyState["ArrowRight"]) this.move("x", "+");
+    if (this.keyState["ArrowLeft"]) this.move("x", "-");
+    if (this.keyState["ArrowUp"]) this.move("y", "-");
+    if (this.keyState["ArrowDown"]) this.move("y", "+");
   }
 
   draw() {
